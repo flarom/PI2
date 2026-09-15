95f29d78db35860c593a418322f266564f5c2c75
flarom <flarocontato@gmail.com>
2026-09-15 19:22:57 -0300

    fix(dialog) remove close dialog button from confirm dialog

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
-import { storage } from './store.js';
+import { storage } from './store.js'; 
 import { dialog }  from './dialog.js';
 
+window.storage = storage;
+window.dialog = dialog;
+
 window.addEventListener('DOMContentLoaded', async () => {
-    console.log("Application started");
+    console.time("Application started");
 
     storage.set("teste", "teste");
     console.log(storage.get("teste"));
 
     //dialog.show('<h1>olá mundo</h1><p>teste de dialog</p>');
-    let teste = await dialog.showFile('dialog/confirm.html', {'title': 'teste'});
+    let teste = await dialog.showFile('dialog/confirm.html', {'title': 'teste', 'content': '<label>teste de dialog</label>'});
     console.log (teste);
+
+    console.timeEnd("Application started");
 });
